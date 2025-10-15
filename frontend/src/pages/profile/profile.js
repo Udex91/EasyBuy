3ef6a77c034172127c04e84678ff663b73371736
Bede Ogbonna <Kceudes2@gmail.com>
2025-10-15 08:47:53 +0100

    update profile components completed

--- frontend/src/pages/profile/profile.js
+++ frontend/src/pages/profile/profile.js
@@ -3,12 +3,14 @@ import "./Profile.scss";
 import PageMenu from '../../components/pageMenu/PageMenu';
 import { useDispatch, useSelector } from 'react-redux';
 import Card from '../../components/card/Card';
-import { getUser, updateUser } from '../../redux/features/auth/authSlice';
+import { getUser, updatePhoto, updateUser } from '../../redux/features/auth/authSlice';
 import Loader from '../../components/loader/Loader';
 import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { toast } from 'react-toastify';
 
-
-
+const cloud_name = process.env.REACT_APP_CLOUD_NAME;
+const upload_preset = process.env.REACT_APP_UPLOAD_PRESET;
+const url = "https://api.cloudinary.com/v1_1/dj2jhmoen/image/upload";
 
 function Profile() {
   const {isLoading, user} = useSelector((state)=> state.auth);
@@ -85,7 +87,41 @@ function Profile() {
     await dispatch(updateUser(userData));
   };
 
-  const savePhoto = async () => {};
+  const savePhoto = async (e) => {
+    e.preventDefault()
+    let imageUrl;
+
+    try {
+      if (
+        profileImage !== null &&
+        (profileImage.type === "image/jpeg" || profileImage.type === "image/jpg" || profileImage.type === "image/png")
+      ) {
+          const image = new FormData()
+          image.append("file", profileImage)
+          image.append("cloud_name", cloud_name)
+          image.append("upload_preset", upload_preset)
+
+          // save image to cloudinary
+          const response = await fetch(url, {
+            method: "POST",
+            body: image,
+          })
+          const imgData = await response.json()
+         // console.log(imgData)
+          imageUrl = imgData.url.toString();
+      }
+
+      // save image to database (mongodb)
+      const userData = {
+        photo: profileImage ? imageUrl : profile.photo
+      }
+      await dispatch(updatePhoto(userData));
+      setImagePreview(null);
+    } catch (error) {
+      toast.error(error.message)
+      
+    }
+  };
   
 
   return (
@@ -101,7 +137,7 @@ function Profile() {
     <div className='--flex-start profile'>
 
       <Card cardClass={"card"}>
-        {!isLoading && user && (
+        {!isLoading  && (
           <>
             <div className='profile-photo'>
               <div>
